4614d93dd6bd74fb663f81bdfe48fd7d9ca7fabf
Jane Kim <jane.kim0717@gmail.com>
2022-02-03 12:38:31 -0500

    commit tree 7f7550d9560c5ae1b6b50e26d08931bd9c8f1ec6

--- index.js
+++ index.js
@@ -1,23 +1,78 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+// const employee = {
+//     name: "Indy",
+//     streetAddress: "1234 Main Street",
+// }
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployee = {...employee};
+//     newEmployee[key] = value;
+//     return newEmployee;
+// }
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee;
+// }
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newEmployee = {...employee};
+//     delete newEmployee[key];
+//     return newEmployee;
+// }
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+//     return employee;
+// }
+
 const employee = {
     name: "Indy",
-    streetAddress: "1234 Main Street",
+    streetAddress: "1617 Doodle Land",
 }
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
     const newEmployee = {...employee};
     newEmployee[key] = value;
     return newEmployee;
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+// function updateEmployeeWithKeyAndValue (employee, key, value) {
+//     return {
+//         ...employee,
+//         [key]: value,
+//     };
+// }
+
+// const newEmployee = updateEmployeeWithKeyAndValue (employee, "name", "Dr. Jones")
+
+
+// console.log (employee);
+// console.log (newEmployee);
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
     employee[key] = value;
     return employee;
 }
-function deleteFromEmployeeByKey(employee, key) {
+
+// const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue (employee, "name", "Mr.Mop")
+
+// console.log(employee);
+// console.log(newEmployee);
+
+function deleteFromEmployeeByKey (employee, key) {
     const newEmployee = {...employee};
     delete newEmployee[key];
     return newEmployee;
 }
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+// const newEmployee = deleteFromEmployeeByKey (employee, "name");
+
+// console.log (employee);
+// console.log (newEmployee);
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
     delete employee[key];
     return employee;
 }
+
+// destructivelyDeleteFromEmployeeByKey (employee, "name");
+
+// console.log (employee);
